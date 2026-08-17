7cee462632b4af04e0b05e666c74938faffe8d31
routh <shej22p@gmail.com>
2026-08-18 01:17:50 +0530

    chore: move unstable branch to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
 	description = "My nixos config after many pinkscreens of death";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
 		home-manager = {
-			url = "github:nix-community/home-manager/release-26.05";
+			url = "github:nix-community/home-manager/";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
