01cc43187a2fe0211f2ea9c6ee78ecda5666777e
routh <shej22p@gmail.com>
2026-08-23 18:50:57 +0530

    fixed the waybar bug

--- modules/desktop/waybar.nix
+++ modules/desktop/waybar.nix
@@ -1,18 +1,9 @@
 { pkgs, ... }:
 
 {
-	programs.waybar.enable = true;
-
-	systemd.user.services.waybar = {
-		description = "Waybar";
-
-		wantedBy = [ "hyprland-session.target"];
-		partOf = ["hyprland-session.target"];
-
-		serviceConfig = {
-			ExexStart = "${pkgs.waybar}/bin/waybar -c /home/routh/nix_os/home/dotfiles/hypr/modules/waybar/config.jsonc -s  /home/routh/nix_os/home/dotfiles/hypr/modules/waybar/style.css"; 
-			Restart = "on-failure";
+	programs.waybar = {
+		enable = true;
+		systemd.target = "hyprland-session.target";
 		};
-	};
 }
 
--- modules/users.nix
+++ modules/users.nix
@@ -4,6 +4,5 @@
  users.users.routh = {
    isNormalUser = true;
    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
- initialPassword = "1770";
  };
 }
