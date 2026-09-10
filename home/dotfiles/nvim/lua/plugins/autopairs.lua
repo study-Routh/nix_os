048f9645b78b44cdea68cb7a90eed0c6cf404cb7
shej <shej22p@gmail.com>
2026-09-11 01:13:59 +0530

    updated the sys and changed nvim lsp to not show error and warning signs inline

--- home/dotfiles/nvim/lua/plugins/autopairs.lua
+++ home/dotfiles/nvim/lua/plugins/autopairs.lua
@@ -1,21 +1,8 @@
 return {
-    "windwp/nvim-autopairs",
-
-    event = "InsertEnter",
-
-    opts = {
-        check_ts = true,
-
-        disable_filetype = {
-            "TelescopePrompt",
-            "oil",
-        },
-
-        map_cr = true,
-        map_bs = true,
-    },
-
-    config = function(_, opts)
-        require("nvim-autopairs").setup(opts)
-    end,
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = { check_ts = true, disable_filetype = { "TelescopePrompt", "oil" }, map_cr = true, map_bs = true },
+	config = function(_, opts)
+		require("nvim-autopairs").setup(opts)
+	end,
 }
--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -139,5 +139,6 @@
 
 #notestaking
             xournalpp
+
             ];
 }
